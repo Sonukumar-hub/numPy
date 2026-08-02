ecd7f662cca9a1e54eb48a2273242b99f90d4ae1
sonu <sonuk625527@gmail.com>
2026-08-02 15:28:14 +0530

    Numpy array iterative

---
+++ 9_numpyArrayIterative.py
@@ -0,0 +1,51 @@
+# iterative array - means going through the element one by one or step by step. like for loop
+
+# iterate element of 1-D
+import numpy as np 
+x = np.array([1,2,3,4])
+
+for i in x:
+    print(i)
+
+
+
+# iterate in 2d array
+
+import numpy as np 
+y = np.array([[1,2,3,4],[5,6,7,8]])
+
+for i in y:
+    for a in i:
+        print(a)
+
+
+
+
+# iterate in 3d array
+
+import numpy as np 
+y = np.array([[[1,2,3,4],[5,6,7,8],[9,10,11,12]]])
+
+for i in y:
+    for a in i:
+        for b in a:
+            print(b)
+
+
+
+# Iterating the array with nditer() function
+
+import numpy as np;
+x = np.array([[[1,2],[3,4],[5,6]]])
+
+for i in np.nditer(x):
+    print(i)
+
+
+# Now we will iterate with different step size
+
+import numpy as np
+
+x = np.array([[1,2,3,4],[5,6,7,8]])
+for i in np.nditer(x[:, ::2]):
+    print(i)
